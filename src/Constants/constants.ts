607b03b4e246c7bbdfd512cce8f7fb04ec8b5d3f
Albert <albert.rocha@r2t.company>
2022-03-07 22:25:50 -0300

    feature: project contants

---
+++ src/Constants/constants.ts
@@ -0,0 +1,34 @@
+import { IUFName } from 'interfaces/IStates';
+
+export const IBGE_URL =
+  'https://servicodados.ibge.gov.br/api/v1/localidades/estados';
+
+export const UF: IUFName = {
+  RO: 'Rondônia',
+  AC: 'Acre',
+  AM: 'Amazonas',
+  RR: 'Roraima',
+  PA: 'Pará',
+  AP: 'Amapá',
+  TO: 'Tocantins',
+  MA: 'Maranhão',
+  PI: 'Piauí',
+  CE: 'Ceará',
+  RN: 'Rio Grande do Norte',
+  PB: 'Paraíba',
+  PE: 'Pernambuco',
+  AL: 'Alagoas',
+  SE: 'Sergipe',
+  BA: 'Bahia',
+  MG: 'Minas Gerais',
+  ES: 'Espírito Santo',
+  RJ: 'Rio de Janeiro',
+  SP: 'São Paulo',
+  PR: 'Paraná',
+  SC: 'Santa Catarina',
+  RS: 'Rio Grande do Sul',
+  MS: 'Mato Grosso do Sul',
+  MT: 'Mato Grosso',
+  GO: 'Goiás',
+  DF: 'Distrito Federal',
+};
