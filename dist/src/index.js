8a7ed3553fd0ff1077b6ed0b9d7daf0ad2d5ad03
Albert <albert.rocha@r2t.company>
2022-03-08 20:52:41 -0300

    feature: new build

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,39 +1,12 @@
-Object.defineProperty(exports, '__esModule', { value: true });
-exports.uniqueStateFile =
-  exports.statesFilesTypescript =
-  exports.statesFilesJson =
-  exports.asyncStateFiles =
-    void 0;
-const asyncStateFile_1 = require('./asyncStateFile');
-
-Object.defineProperty(exports, 'asyncStateFiles', {
-  enumerable: true,
-  get() {
-    return asyncStateFile_1.asyncStateFiles;
-  },
-});
-const statesFilesJson_1 = require('./statesFilesJson');
-
-Object.defineProperty(exports, 'statesFilesJson', {
-  enumerable: true,
-  get() {
-    return statesFilesJson_1.statesFilesJson;
-  },
-});
-const statesFilesTypescript_1 = require('./statesFilesTypescript');
-
-Object.defineProperty(exports, 'statesFilesTypescript', {
-  enumerable: true,
-  get() {
-    return statesFilesTypescript_1.statesFilesTypescript;
-  },
-});
-const uniqueStateFile_1 = require('./uniqueStateFile');
-
-Object.defineProperty(exports, 'uniqueStateFile', {
-  enumerable: true,
-  get() {
-    return uniqueStateFile_1.uniqueStateFile;
-  },
-});
-// # sourceMappingURL=index.js.map
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.uniqueStateFile = exports.statesFilesTypescript = exports.statesFilesJson = exports.asyncStateFiles = void 0;
+const asyncStateFile_1 = require("./asyncStateFile");
+Object.defineProperty(exports, "asyncStateFiles", { enumerable: true, get: function () { return asyncStateFile_1.asyncStateFiles; } });
+const statesFilesJson_1 = require("./statesFilesJson");
+Object.defineProperty(exports, "statesFilesJson", { enumerable: true, get: function () { return statesFilesJson_1.statesFilesJson; } });
+const statesFilesTypescript_1 = require("./statesFilesTypescript");
+Object.defineProperty(exports, "statesFilesTypescript", { enumerable: true, get: function () { return statesFilesTypescript_1.statesFilesTypescript; } });
+const uniqueStateFile_1 = require("./uniqueStateFile");
+Object.defineProperty(exports, "uniqueStateFile", { enumerable: true, get: function () { return uniqueStateFile_1.uniqueStateFile; } });
+//# sourceMappingURL=index.js.map
