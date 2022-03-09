2985e8ff2c53fe204d7aa09aeef2deabec1f98b3
Albert <albert.rocha@r2t.company>
2022-03-09 20:04:42 -0300

    improvement: changed file path

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { asyncStateFiles } from './asyncStateFile';
+import { statesFilesJson } from './statesFilesJson';
+import { statesFilesTypescript } from './statesFilesTypescript';
+import { uniqueStateFile } from './uniqueStateFile';
+
+export {
+  asyncStateFiles,
+  statesFilesJson,
+  statesFilesTypescript,
+  uniqueStateFile,
+};
