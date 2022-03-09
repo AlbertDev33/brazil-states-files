5c002270e080cba43fb04ecd61a5cb2c170ffe0c
Albert <albert.rocha@r2t.company>
2022-03-08 22:56:27 -0300

    feature: dist files update

--- dist/src/asyncStateFile.js
+++ dist/src/asyncStateFile.js
@@ -9,33 +9,44 @@ const fs_1 = __importDefault(require("fs"));
 const util_1 = require("util");
 const constants_1 = require("./constants/constants");
 const writeFile = (0, util_1.promisify)(fs_1.default.writeFile);
+const fetchStates = async (url) => {
+    const { data } = await axios_1.default.get(url);
+    return data;
+};
 async function asyncStateFiles() {
+    const promise = [];
     let length = Object.keys(constants_1.UF).length - 1;
     const keys = Object.keys(constants_1.UF);
     const newState = {};
     while (length !== -1) {
         const uf = keys[length];
         const url = `${constants_1.IBGE_URL}/${uf}/municipios`;
-        const { data } = await axios_1.default.get(url);
-        data.map(async (state) => {
-            const stateName = state.microrregiao.mesorregiao.UF.nome;
+        promise.push(fetchStates(url));
+        length -= 1;
+    }
+    const data = await Promise.all(promise);
+    data.map(async (state) => {
+        state.forEach(async (region) => {
+            const stateName = region.microrregiao.mesorregiao.UF.nome;
             const citiesName = newState[stateName];
             if (citiesName) {
-                citiesName.push({ city: state.nome });
+                citiesName.push({ city: region.nome });
             }
             else {
-                newState[stateName] = [{ city: state.nome }];
+                newState[stateName] = [{ city: region.nome }];
             }
             const parseCities = JSON.stringify(newState[stateName]);
             const trimStateName = stateName.replaceAll(' ', '');
-            await writeFile(`${trimStateName}.ts`, JSON.parse(JSON.stringify(`export const ${trimStateName} = ${parseCities}`)));
-            return newState;
+            const treatStateName = stateName
+                .replaceAll(' ', '')
+                .normalize('NFD')
+                .replace(/[\u0300-\u036f]/g, '');
+            await writeFile(`${trimStateName}.ts`, JSON.parse(JSON.stringify(`export const ${treatStateName} = ${parseCities}`)));
+            const fileState = fs_1.default.createReadStream(`${trimStateName}.ts`);
+            fileState.pipe(fs_1.default.createWriteStream('States.ts'));
         });
-        length -= 1;
-        const stateName = constants_1.UF[uf].replaceAll(' ', '');
-        const fileState = fs_1.default.createReadStream(`${stateName}.ts`);
-        fileState.pipe(fs_1.default.createWriteStream('States.ts'));
-    }
+        return newState;
+    });
 }
 exports.asyncStateFiles = asyncStateFiles;
 //# sourceMappingURL=asyncStateFile.js.map
--- dist/src/statesFilesTypescript.js
+++ dist/src/statesFilesTypescript.js
@@ -12,31 +12,39 @@ const fetchStates = async (url) => {
     return data;
 };
 async function statesFilesTypescript() {
+    const promise = [];
     let length = Object.keys(constants_1.UF).length - 1;
     const keys = Object.keys(constants_1.UF);
     while (length !== -1) {
         const uf = keys[length];
         const url = `${constants_1.IBGE_URL}/${uf}/municipios`;
-        const data = await fetchStates(url);
-        data.reduce((statesAcc, ibgeData) => {
-            let acc = {};
-            acc = statesAcc;
-            const stateName = ibgeData.microrregiao.mesorregiao.UF.nome;
+        promise.push(fetchStates(url));
+        length -= 1;
+    }
+    const data = await Promise.all(promise);
+    data.reduce((statesAcc, ibgeData) => {
+        let acc = {};
+        acc = statesAcc;
+        ibgeData.forEach(region => {
+            const stateName = region.microrregiao.mesorregiao.UF.nome;
             const state = acc[stateName] || [];
-            const city = { city: ibgeData.nome };
+            const city = { city: region.nome };
             state.push(city);
             acc[stateName] = state;
             const parseCities = JSON.stringify(state);
             const trimStateName = stateName.replaceAll(' ', '');
-            const file = (0, fs_1.createWriteStream)(`./${stateName}.ts`);
+            const treatStateName = stateName
+                .replaceAll(' ', '')
+                .normalize('NFD')
+                .replace(/[\u0300-\u036f]/g, '');
+            const file = (0, fs_1.createWriteStream)(`./${trimStateName}.ts`);
             file.once('open', () => {
-                file.write(JSON.parse(JSON.stringify(`export const ${trimStateName} = ${parseCities}`)));
+                file.write(JSON.parse(JSON.stringify(`export const ${treatStateName} = ${parseCities}`)));
                 file.end();
             });
-            return acc;
-        }, {});
-        length -= 1;
-    }
+        });
+        return acc;
+    }, {});
 }
 exports.statesFilesTypescript = statesFilesTypescript;
 //# sourceMappingURL=statesFilesTypescript.js.map
